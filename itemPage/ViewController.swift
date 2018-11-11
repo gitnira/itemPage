3f7ee901474c628e3afbf4f8445fe401563f7d71
gitnira <tyukasanLSBS@gmail.com>
2018-11-11 15:32:10 +0900

    Initial Commit

---
+++ itemPage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  itemPage
+//
+//  Created by nira on 2018/11/11.
+//  Copyright © 2018年 nira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
